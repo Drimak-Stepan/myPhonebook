30c06e84ceacfce43a62e6c1762f5d7630f9e273
Stepan Drimak <108022018+Drimak-Stepan@users.noreply.github.com>
2023-01-24 12:28:23 +0200

    hw-3-1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -42,6 +42,20 @@ class App extends Component {
     }));
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   render() {
     const { filter } = this.state;
     const visibleContacts = this.getVisibleContacts();
@@ -62,4 +76,3 @@ class App extends Component {
 }
 
 export default App;
-
